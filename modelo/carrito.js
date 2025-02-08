846f645a932888775aec1024715ef43e3d626a4f
Carlos Campos <71299504+NoobMaster-69-UwU@users.noreply.github.com>
2025-02-07 23:06:40 -0600

    Actualizacion
    Definición de clases a usar y posibles métodos a utilizar para el carrito

---
+++ modelo/carrito.js
@@ -0,0 +1,29 @@
+export class Carrito{
+    constructor(){
+        this.productos;
+    }
+
+
+    //Metodos del Carrito de Compras
+
+    agregarProducto(){
+
+    }
+
+    eliminarProducto(){
+
+    }
+
+    actualizarCantProducto (){
+
+    }
+
+    calcTotal(){
+
+    }
+
+    limpiarCarrito(){
+        
+    }
+}
+
