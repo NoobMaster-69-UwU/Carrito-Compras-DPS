bec6f9fd70d050f973a07f472f187cb082691589
FsquijadaA <127255812+FsquijadaA@users.noreply.github.com>
2025-02-10 21:00:49 -0600

    Aplicar IVA y calcular total con IVA
    Se crea el método de calcular precio con iva y el método de calcular total

--- modelo/carrito.js
+++ modelo/carrito.js
@@ -1,6 +1,6 @@
 export class Carrito{
     constructor(){
-        this.productos;
+        this.productos = [];
     }
 
 
@@ -18,7 +18,14 @@ export class Carrito{
 
     }
 
+    // Método para calcular el precio con IVA
+    calcularPrecioConIVA() {
+        const IVA = 0.13; // 13% de IVA
+        return this.precio * (1 + IVA);
+    }
+
     calcTotal(){
+        return this.calcularPrecioConIVA() * this.cantidad;
 
     }
 
