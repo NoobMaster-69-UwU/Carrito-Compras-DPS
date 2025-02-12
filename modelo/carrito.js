934b433ea2384d1bdfb0cf78464ce205be6154c2
Ludwin Martínez <ludwin604@gmail.com>
2025-02-11 23:35:47 -0600

    First steps to make the cart functional

--- modelo/carrito.js
+++ modelo/carrito.js
@@ -6,16 +6,27 @@ export class Carrito{
 
     //Metodos del Carrito de Compras
 
-    agregarProducto(){
-
+    agregarProducto(producto){
+        const ProductoExistente = this.productos.find(item => item.nombre === producto.nombre);
+        if (ProductoExistente){
+            ProductoExistente.aumentarCantidad();
+        } else {
+            this.productos.push(producto);
+        }
     }
 
-    eliminarProducto(){
-
+    eliminarProducto(nombreProd){
+        this.productos = this.productos.filter(item => item.nombre !== nombreProd);
     }
 
     actualizarCantProducto (){
 
+    }
+
+    // Método para calcular el precio con IVA
+    calcularPrecioConIVA() {
+        const IVA = 0.13; // 13% de IVA
+        return this.precio * (1 + IVA);
     }
 
     calcTotal(){
--- modelo/producto.js
+++ modelo/producto.js
@@ -4,10 +4,26 @@ export class Producto{
     
     constructor(id, nombre, imagen, precio, cantidad){
         this.id = id;
-        this.nombreProd = nombre;
+        this.nombre = nombre;
         this.imagen = imagen;
         this.precio = precio;
         this.cantidad = cantidad;
     }
-}
 
+    aumentarCantidad(){
+        this.cantidad += 1;
+        return this.cantidad;
+    }
+
+    disminuirCantidad() {
+        if (this.cantidad > 1) {
+            this.cantidad -= 1;
+            return this.cantidad;
+        }
+        return 0; // Indica que el producto debe eliminarse
+    }
+    
+    obtenerPrecioTotal() {
+        return this.precio * this.cantidad;
+    }
+}
